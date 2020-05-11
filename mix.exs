7844ae397c6026fd42bbec4ae81a10813a6361ad
ian knauer <ian.knauer@gmail.com>
2020-05-11 11:21:17 -0700

    update gitignore and mix

--- mix.exs
+++ mix.exs
@@ -33,16 +33,17 @@ defmodule Api.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.11"},
-      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix, "~> 1.5.0-rc.0", override: true},
+      {:phoenix_pubsub, "~> 2.0"},
+      {:phoenix_live_dashboard, "~> 0.1"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 2.14.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
+      {:plug_cowboy, "~> 2.1"},
       {:ja_serializer, github: "vt-elixir/ja_serializer"},
       {:cors_plug, "~> 1.2"},
       {:comeonin, "~>4.0"},
@@ -50,7 +51,10 @@ defmodule Api.MixProject do
       {:joken, "~> 1.5.0"},
       {:poison, "~> 4.0.1", override: true},
       {:bamboo, "~> 1.2"},
-      {:triplex, "~> 1.3.0"}
+      {:triplex, "~> 1.3.0"},
+      {:puppeteer_pdf, "~> 1.0.3"},
+      {:tesla, "~> 1.3.0"},
+      {:hackney, "~> 1.15.2"}
     ]
   end
 
